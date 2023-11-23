8c3f10c501d51f80777a4d575799df3553121b08
CarlosSanz1505 <csanchezor@unal.edu.co>
2023-11-23 16:57:39 -0500

    Hard-Reset, pygame approach

--- REACT Approach/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-const domNode = document.getElementById('root');
-const root = ReactDOM.createRoot(domNode);
-root.render(<App />);
